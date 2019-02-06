8a8ffccad2ff73200e1a6a6e89a31d855a282d95
kong.zeya <kong.zeya@gmail.com>
2019-02-06 14:44:25 -0600

    add contents

--- hw1/server/HttpRequestImpl.java
+++ hw1/server/HttpRequestImpl.java
@@ -2,106 +2,117 @@ package hw1.server;
 
 import hw1.lib.HttpRequest;
 
+import java.util.HashMap;
 import java.util.Set;
 
 public class HttpRequestImpl implements HttpRequest {
+    private String method;
+    private String version;
+    private HashMap<String,String> query = new HashMap<>();
+    private String path;
+    private String url;
+    private String host;
+    private String body;
+    private Integer port;
+    private HashMap<String,String> header = new HashMap<>();
+
     @Override
     public void setMethod(String method) {
-
+        this.method = method;
     }
 
     @Override
     public void setVersion(String version) {
-
+        this.version = version;
     }
 
     @Override
     public void setPath(String path) {
-
+        this.path = path;
     }
 
     @Override
     public void setQuery(String key, String value) {
-
+        query.put(key,value);
     }
 
     @Override
     public void setHeader(String key, String value) {
-
+        header.put(key,value);
     }
 
     @Override
     public void setHost(String host) {
-
+        this.host = host;
     }
 
     @Override
     public void setBody(String body) {
-
+        this.body = body;
     }
 
     @Override
     public void setPort(Integer port) {
-
+        this.port = port;
     }
 
     @Override
     public void setUrl(String url) {
-
+        this.url = url;
     }
 
     @Override
     public String getUrl() {
-        return null;
+        return url;
     }
 
     @Override
     public String getMethod() {
-        return null;
+        return method;
     }
 
     @Override
     public String getVersion() {
-        return null;
+        return version;
     }
 
     @Override
     public String getPath() {
-        return null;
+        return path;
     }
 
     @Override
     public String getHeader(String key) {
-        return null;
+        return header.get(key);
     }
 
     @Override
     public Set<String> getHeaderNames() {
-        return null;
+        return header.keySet();
     }
 
     @Override
     public String getQuery(String key) {
-        return null;
+        return query.get(key);
     }
 
     @Override
     public Set<String> getQueryNames() {
-        return null;
+        return query.keySet();
     }
 
     @Override
     public String getHost() {
-        return null;
+        return host;
     }
 
     @Override
     public String getBody() {
-        return null;
+        return body;
     }
 
     @Override
     public Integer getPort() {
-        return null;
+        return port;
     }
 }
--- hw1/server/HttpResponseImpl.java
+++ hw1/server/HttpResponseImpl.java
@@ -2,61 +2,68 @@ package hw1.server;
 
 import hw1.lib.HttpResponse;
 
+import java.util.HashMap;
 import java.util.Set;
 
 public class HttpResponseImpl implements HttpResponse {
+    private String version;
+    private String statusCode;
+    private HashMap<String,String> header = new HashMap<>();
+    private String description;
+    private String body;
+
     @Override
     public String getVersion() {
-        return null;
+        return version;
     }
 
     @Override
     public String getStatusCode() {
-        return null;
+        return statusCode;
     }
 
     @Override
     public String getDescription() {
-        return null;
+        return description;
     }
 
     @Override
     public String getHeader(String key) {
-        return null;
+        return header.get(key);
     }
 
     @Override
     public Set<String> getHeaderNames() {
-        return null;
+        return header.keySet();
     }
 
     @Override
     public String getBody() {
-        return null;
+        return body;
     }
 
     @Override
     public void setVersion(String version) {
-
+        this.version =version;
     }
 
     @Override
     public void setStatusCode(String statusCode) {
-
+        this.statusCode = statusCode;
     }
 
     @Override
     public void setDescription(String description) {
-
+        this.description = description;
     }
 
     @Override
     public void setBody(String body) {
-
+        this.body = body;
     }
 
     @Override
     public void setHeader(String key, String value) {
-
+        header.put(key,value);
     }
 }
