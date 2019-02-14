42a1218537f2a9fc3d9f89f8fe4261049325a441
kong.zeya <kong.zeya@gmail.com>
2019-02-13 19:42:53 -0600

    add contents

--- hw2/routes/index.js
+++ hw2/routes/index.js
@@ -1,6 +1,41 @@
 var express = require('express');
+var uuid = require('uuid');
 var router = express.Router();
 
+//################ variables ####################
+var font = {
+    category: "",
+    family: "",
+    link: ""
+};
+
+var theme = {
+    font: font,
+    color: "",
+    playerToken: "",
+    computerToken: ""
+};
+
+var metadata = {
+    fonts: [],
+    tokens: [],
+    default: theme
+};
+
+var game = {
+    theme: theme,
+    id: "",
+    unoccupied: 123,
+    status: "unfinished",
+    timestamp: 123,
+    timeToComplete: 123,
+    view: [[]]
+};
+var error = {
+    msg: ""
+};
+
+//################ routers ######################
 //return the whole html index file( the whole app)
 router.get('/connectfour', function (req, res, next) {
     res.render('index');
@@ -8,12 +43,15 @@ router.get('/connectfour', function (req, res, next) {
 
 //get the sid from the response
 router.get('/connectfour/api/v1/sids', function (req, res, next) {
-
+    res.send(uuid.v4());
 });
 
 //get a meta object from the response
 router.get('/connectfour/api/v1/meta', function (req, res, next) {
-
+    metadata.fonts = "";
+    metadata.default.color = "red";
+    metadata.default.font = font;
+    res.send(metadata)
 });
 
 //get a list of font objects: [font]
