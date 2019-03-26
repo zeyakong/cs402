6e13313bb3aef2ba269fb0e198aa0ca4acb5b162
kong.zeya <kong.zeya@gmail.com>
2019-03-25 22:54:10 -0500

    add mongodb driver

--- hw3/app.js
+++ hw3/app.js
@@ -2,11 +2,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/hw3';
+
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    db.close();
+});
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
